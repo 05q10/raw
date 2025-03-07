3c49b2bac7f66a8b01273a3061700b3ae135f397
Ria Talsania <riatalsania0510@gmail.com>
2025-03-07 23:15:50 +0530

    videotest

---
+++ client/src/components/Video.js
@@ -0,0 +1,26 @@
+// src/Video.js
+import React, { useState } from 'react';
+import myVideo from './path-to-your-video-file.mp4';
+
+const Video = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  return (
+    <div>
+      {isLoading && <div>Loading...</div>}
+      <video
+        src={myVideo}
+        autoPlay
+        loop
+        muted
+        preload="auto"
+        style={{ width: '100%', height: 'auto' }}
+        onCanPlayThrough={() => setIsLoading(false)}
+      >
+        Your browser does not support the video tag.
+      </video>
+    </div>
+  );
+};
+
+export default Video;
